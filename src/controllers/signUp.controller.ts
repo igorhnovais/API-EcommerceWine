746c3509fc8b98b9120a47813e562b6ca9aade3d
Igor Novais <igorhnovaisdriven@gmail.com>
2023-02-22 15:38:23 -0300

    refact

--- src/controllers/signUp.controller.ts
+++ src/controllers/signUp.controller.ts
@@ -3,6 +3,7 @@ import { users } from "../protocols/index.js";
 import usersServices from "../services/index.js";
 
 export async function signUp (req:Request, res: Response){
+    
     const {email, name, password}= res.locals.info as users;
 
     try{
