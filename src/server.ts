31711e20770cdd9519afb689feb3fde260956328
Igor Novais <igorhnovaisdriven@gmail.com>
2023-02-21 21:49:22 -0300

    feat: server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from "cors";
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use();
+
+
+const port: number  = +process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
