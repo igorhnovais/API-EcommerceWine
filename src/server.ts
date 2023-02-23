0aabd84b7bbeece553e3de9c1062a0c056909c8c
Igor Novais <igorhnovaisdriven@gmail.com>
2023-02-22 21:05:56 -0300

    refact

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import express from 'express';
-import cors from "cors";
-
-import userRouters from "./routes/user.route.js"
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use(userRouters);
+import app from "./app.js";
 
 
 const port: number  = +process.env.PORT || 4000;
