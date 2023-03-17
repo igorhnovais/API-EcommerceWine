2dac7898aa6506b05fbe875959d1092ea861192e
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-17 16:24:01 -0300

    feat: product router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from "cors";
 
-import userRouters from "./routes/user.route"
+import userRouters from "./routes/user.route";
+import productsRouters from "./routes/products.route"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(userRouters);
+app.use(productsRouters);
 
 
 
