a0389972e4b442e1d0427e3a57ceba1e08f49f37
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-17 16:24:55 -0300

    feat: /products

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,1 +1,10 @@
 import { Router } from "express";
+
+import { getProducts } from "../controllers/products/getProducts.controller";
+
+const router = Router();
+
+router.get("/products", getProducts);
+
+
+export default router;
