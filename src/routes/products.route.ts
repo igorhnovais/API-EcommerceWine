93d87a65bb0ce313138c7f62c2eb0f1b9ab1d476
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-18 18:36:35 -0300

    feat: router "/products/wine"

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { getProducts } from "../controllers/products/getProducts.controller";
+import { getOneProduct } from "../controllers/products/getOneProduct.controller"
 
 const router = Router();
 
 router.get("/products", getProducts);
+router.get("/products/wine", getOneProduct);
 
 
 export default router;
