5041d965a12fdc5bcd9c40944b6f1aaca444c362
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-18 18:36:07 -0300

    feat: function getOneProduct

---
+++ src/controllers/products/getOneProduct.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import productsService from "../../services/products";
+
+export async function getOneProduct(req:Request, res: Response){
+    const {id} = req.headers;
+    const idProduct = Number(id);
+
+    try{
+        const oneProduct = await productsService.findOneProduct(idProduct);
+        res.send(oneProduct);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(err.status).send(err.message);
+    }
+}
