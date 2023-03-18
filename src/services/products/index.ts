4614780b29f018f602f52875bcfbbc00c5bf77f5
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-18 18:34:59 -0300

    feat: function findOneProduct

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -1,12 +1,30 @@
 import productsRepositories from "../../repositories/products";
+import { notFound } from "../../errors";
 
 async function findProduts(){
     const products = await productsRepositories.findmanyProducts();
     return products;
 }
 
+async function findOneProduct(id:number){
+
+    if (!id){
+        throw notFound();
+    }
+
+    const product = await productsRepositories.findFirstProduct(id);
+
+    if(!product){
+        throw notFound()
+    }
+
+    return product;
+
+}
+
 const productsService = {
-    findProduts
+    findProduts,
+    findOneProduct
 }
 
 export default productsService;
