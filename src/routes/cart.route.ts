674c219896ae139cf46629775c18a18c2014e864
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-23 23:55:29 -0300

    feat: route delete "/cart"

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 
 import {postOneProduct} from "../controllers/cart/postOneProduct.controller";
 import { getProductsCart } from "../controllers/cart/getProductsCart.controller";
+import { deleteProductCart } from "../controllers/cart/deleteProductCart.controller";
 import { authVerification } from "../middlewares/authVerification.middleware";
 
 
 const router = Router();
 
 router.post("/cart", authVerification, postOneProduct);
-router.get("/cart", getProductsCart )
+router.get("/cart", getProductsCart );
+router.delete("/cart", deleteProductCart)
 
 
 export default router;
