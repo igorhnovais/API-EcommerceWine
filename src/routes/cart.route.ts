a94b1990ea0b358e2b5a18fdedd8190834dc40cf
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-24 18:45:16 -0300

    feat: route "/cart-balance

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -4,14 +4,16 @@ import {postOneProduct} from "../controllers/cart/postOneProduct.controller";
 import { getProductsCart } from "../controllers/cart/getProductsCart.controller";
 import { deleteProductCart } from "../controllers/cart/deleteProductCart.controller";
 import { authVerification } from "../middlewares/authVerification.middleware";
+import { getBalanceProducts } from "../controllers/cart/getBalanceProducts.controller"
 
 
 const router = Router();
 
-router.all("/*", authVerification);
+//router.all("/*", authVerification);
 router.post("/cart", postOneProduct);
 router.get("/cart", getProductsCart );
-router.delete("/cart/:id", deleteProductCart)
+router.delete("/cart/:id", deleteProductCart);
+router.get("cart-balance", getBalanceProducts);
 
 
 export default router;
