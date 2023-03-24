67802a44f9a271fc2ee9b317f43628583ed8fc03
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-24 18:45:48 -0300

    feat

---
+++ src/controllers/cart/getBalanceProducts.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import cartService from "../../services/carts";
+
+export async function getBalanceProducts(req:Request, res: Response){
+    //const userId: number = res.locals.info;
+    const userId = 2
+
+    try{
+
+        const balance = cartService.getBalanceCart(userId);
+        res.status(200).send(balance);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(err.status).send(err.message);
+    }
+}
