93e6fe9010697afa297b04ebbc01d8ee1c2c1f11
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-24 23:43:56 -0300

    feat: route delete "/session"

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 
 import { signUp } from "../controllers/users/signUp.controller";
 import { signIn } from "../controllers/users/signIn.controller";
+import { logOut } from "../controllers/users/logOut.controller";
 import { emailValidation } from "../middlewares/emailValidation.middleware";
 import { userValidation } from "../middlewares/userValidation.middleware";
+import { authVerification } from "../middlewares/authVerification.middleware";
+
 
 const router = Router();
 
 router.post("/sign-up", emailValidation, signUp);
 router.post("/sign-in", userValidation, signIn);
+router.delete("/session", authVerification, logOut)
 
 export default router;
