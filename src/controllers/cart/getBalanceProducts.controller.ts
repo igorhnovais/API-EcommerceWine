90abaf7b5b538176c5206b7d8b1ba1a49439a5f5
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-25 13:07:47 -0300

    refact: remove id fixed

--- src/controllers/cart/getBalanceProducts.controller.ts
+++ src/controllers/cart/getBalanceProducts.controller.ts
@@ -3,10 +3,8 @@ import cartService from "../../services/carts";
 
 export async function getBalanceProducts(req:Request, res: Response){
     const userId: number = res.locals.info;
-    //const userId = 2
 
     try{
-
         const balance = await cartService.getBalanceCart(userId);
         res.status(200).send(balance);
 
