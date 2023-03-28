bec90f81205ca1d9110231186b4edb8fc92765fc
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-28 19:36:07 -0300

    feat: stripe route

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import cors from "cors";
 import userRouters from "./routes/user.route";
 import productsRouters from "./routes/products.route";
 import cartsRouter from "./routes/cart.route";
+import { webhookRouter } from './routes/webhook.route';
 
 const app = express();
 app.use(cors());
+app.use("/webhook", express.raw({ type: "application/json" }), webhookRouter)
 app.use(express.json());
 
 app.use(userRouters);
