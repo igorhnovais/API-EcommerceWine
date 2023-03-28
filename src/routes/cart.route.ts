43369326b3de1bb3a96ecc7eba7f379ddb4d9894
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-28 19:38:42 -0300

    feat: post "/create-checkout-session"

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -4,7 +4,8 @@ import {postOneProduct} from "../controllers/cart/postOneProduct.controller";
 import { getProductsCart } from "../controllers/cart/getProductsCart.controller";
 import { deleteProductCart } from "../controllers/cart/deleteProductCart.controller";
 import { authVerification } from "../middlewares/authVerification.middleware";
-import { getBalanceProducts } from "../controllers/cart/getBalanceProducts.controller"
+import { getBalanceProducts } from "../controllers/cart/getBalanceProducts.controller";
+import { postPaymentStripe } from "../controllers/cart/postPaymentStripe.controller";
 
 
 const router = Router();
@@ -14,6 +15,7 @@ router.post("/cart", postOneProduct);
 router.get("/cart", getProductsCart );
 router.delete("/cart/:id", deleteProductCart);
 router.get("/cart-balance", getBalanceProducts);
+router.post("/create-checkout-session", postPaymentStripe)
 
 
 export default router;
