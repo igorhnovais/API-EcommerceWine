2b680ce44416bc87251774c3291d97565dfa58e9
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-30 17:49:54 -0300

    feat: "/products/taboo"

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -4,6 +4,7 @@ import { getProducts } from "../controllers/products/getProducts.controller";
 import { getOneProduct } from "../controllers/products/getOneProduct.controller";
 import { getwine } from "../controllers/products/getWine.controller";
 import { getCups } from "../controllers/products/getCups.controller";
+import { getTaboos } from "../controllers/products/getTaboos.controller";
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.get("/products", getProducts);
 router.get("/product/:id", getOneProduct);
 router.get("/products/wine", getwine);
 router.get("/products/cup", getCups);
+router.get("/products/taboo", getTaboos);
 
 
 
