19e9028cd86fddcdaf895d53b6840ef43907a516
Igor Novais <igorhnovaisdriven@gmail.com>
2023-03-31 20:17:32 -0300

    feat: route "/products/searched"

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -5,6 +5,7 @@ import { getOneProduct } from "../controllers/products/getOneProduct.controller"
 import { getwine } from "../controllers/products/getWine.controller";
 import { getCups } from "../controllers/products/getCups.controller";
 import { getTaboos } from "../controllers/products/getTaboos.controller";
+import { getSearchedProducts } from "../controllers/products/getSearchedProducts";
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.get("/product/:id", getOneProduct);
 router.get("/products/wine", getwine);
 router.get("/products/cup", getCups);
 router.get("/products/taboo", getTaboos);
+router.post("/products/searched", getSearchedProducts)
 
 
 
