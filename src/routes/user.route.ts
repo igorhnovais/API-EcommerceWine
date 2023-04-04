61918cff87eb4446426a4ae2951ec8345d88158e
Igor Novais <igorhnovaisdriven@gmail.com>
2023-04-04 16:14:03 -0300

    feat: route get "/adm"

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,12 +6,14 @@ import { logOut } from "../controllers/users/logOut.controller";
 import { emailValidation } from "../middlewares/emailValidation.middleware";
 import { userValidation } from "../middlewares/userValidation.middleware";
 import { authVerification } from "../middlewares/authVerification.middleware";
+import { getAdm } from "../controllers/users/getAdm.controller";
 
 
 const router = Router();
 
 router.post("/sign-up", emailValidation, signUp);
 router.post("/sign-in", userValidation, signIn);
-router.delete("/session", authVerification, logOut)
+router.delete("/session", authVerification, logOut);
+router.get("/adm", authVerification, getAdm);
 
 export default router;
