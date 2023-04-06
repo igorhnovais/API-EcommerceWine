d9fd7b2b98a09c9b2f448e6644109c22d48dce72
Igor Novais <igorhnovaisdriven@gmail.com>
2023-04-05 23:15:50 -0300

    feat: type product

--- src/protocols/index.ts
+++ src/protocols/index.ts
@@ -1,8 +1,17 @@
 export type users = {
-    id?: string
+    id?: string,
     email: string,
     name?: string,
     password: string,
     confirmPassword?:string
 };
 
+export type product = {
+    name: string,
+    image: string,
+    description?: string,
+    type?: string,
+    alcohol?: string,
+    value: number,
+    type_product?: string,
+}
