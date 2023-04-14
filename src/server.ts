22f8f88b906a69bf2eedc3257e5c31b0fa625adc
Igor Novais <igorhnovaisdriven@gmail.com>
2023-04-13 21:01:46 -0300

    refact: port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
 
-const port: number  = +process.env.PORT || 4000;
+const port: number  = +process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running in port: ${port}`));
